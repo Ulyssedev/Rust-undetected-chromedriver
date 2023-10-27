4d8ba6b84a5c7ec121ce03d5c2d1e8d74256fc8c
Chung Wong <wch705@gmail.com>
2023-10-27 11:25:47 +1100

    refactor

--- tests/chrome.rs
+++ tests/chrome.rs
@@ -1,11 +1,14 @@
 #[cfg(test)]
 mod tests {
-    use undetected_chromedriver::chrome;
+    use std::error::Error;
+    use undetected_chromedriver::chrome_driver;
 
     #[tokio::test]
-    async fn test_chrome() {
-        let driver = chrome().await.unwrap();
+    async fn test_chrome() -> Result<(), Box<dyn Error>> {
+        let driver = chrome_driver().await?;
         assert!(driver.title().await.is_ok());
-        driver.quit().await.unwrap();
+        driver.quit().await?;
+
+        Ok(())
     }
 }
--- tests/recaptcha.rs
+++ tests/recaptcha.rs
@@ -1,29 +1,35 @@
 #[cfg(test)]
 mod tests {
-    use thirtyfour::prelude::{ElementQueryable, ElementWaitable};
-    use thirtyfour::By;
-    use undetected_chromedriver::chrome;
+    use std::error::Error;
+    use thirtyfour::{
+        prelude::{ElementQueryable, ElementWaitable},
+        By,
+    };
+    use undetected_chromedriver::chrome_driver;
 
-    async fn get_score(driver: &thirtyfour::WebDriver) -> Option<f32> {
+    async fn get_score(driver: &thirtyfour::WebDriver) -> Result<f32, Box<dyn Error>> {
         driver
             .goto("https://recaptcha-demo.appspot.com/recaptcha-v3-request-scores.php")
-            .await
-            .unwrap();
+            .await?;
+
         let button = driver
             .query(By::XPath(r#"//*[@id="recaptcha-steps"]/li[2]/button[2]"#))
             .first()
-            .await
-            .unwrap();
-        button.wait_until().clickable().await.unwrap();
-        button.click().await.unwrap();
+            .await?;
+
+        button.wait_until().clickable().await?;
+        button.click().await?;
+
         let response = driver
             .query(By::XPath(r#"//*[@id="recaptcha-steps"]/li[5]/pre"#))
             .first()
-            .await
-            .unwrap();
-        response.wait_until().displayed().await.unwrap();
-        println!("reponse: {}", response.text().await.unwrap());
-        let response_text = response.text().await.unwrap();
+            .await?;
+
+        response.wait_until().displayed().await?;
+
+        println!("reponse: {}", response.text().await?);
+
+        let response_text = response.text().await?;
         let score = response_text
             .lines()
             .find(|line| line.contains("\"score\":"))
@@ -33,14 +39,17 @@ mod tests {
                 line.get(start_index + 1..end_index)
             })
             .and_then(|score_str| score_str.trim().parse::<f32>().ok());
-        score
+
+        Ok(score.ok_or("cannot find score")?)
     }
 
     #[tokio::test]
-    async fn recaptcha() {
-        let driver = chrome().await.unwrap();
+    async fn recaptcha() -> Result<(), Box<dyn Error>> {
+        let driver = chrome_driver().await?;
         let score = get_score(&driver).await;
         assert!(score.unwrap_or(0.0) >= 0.7);
-        driver.quit().await.unwrap();
+        driver.quit().await?;
+
+        Ok(())
     }
 }
